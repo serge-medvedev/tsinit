92bc67ae1997315053e7c153fca02d03711bb03e
Serge Medvedev <thismoment.main@gmail.com>
2022-07-16 11:30:41 +0300

    chore: tslint added

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,17 @@ set -euxo pipefail
 npm init -y > /dev/null
 jq '.main = "dist/index.js"' package.json | tee package.json.new
 mv package.json.new package.json
-npm install --save-dev typescript
+npm install --save-dev typescript tslint
 mkdir -p src
 touch src/index.ts
 npx tsc --init --rootDir src --outDir dist
+npx tslint --init
 npm install --save-dev \
     @types/node \
     nodemon \
     ts-node
-npm set-script build "tsc"
-npm set-script start "node dist/index.js"
-npm set-script dev "nodemon --watch 'src/**/*.ts' --exec 'ts-node' src/index.ts"
+npm pkg set scripts.build="tsc"
+npm pkg set scripts.start="node dist/index.js"
+npm pkg set scripts.dev="nodemon --watch 'src/**/*.ts' --exec 'ts-node' src/index.ts"
+npm pkg set scripts.lint="tslint -c tslint.json 'src/**/*.ts'"
 
