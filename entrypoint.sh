b23c8c69bf6437f6a0ea6465ec35ac5097706ea7
Serge Medvedev <thismoment.main@gmail.com>
2022-07-18 11:48:34 +0300

    chore: tslint got replaced via eslint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,33 @@ PROJECT=${1:-app}
 set -euxo pipefail
 
 npm init -y > /dev/null
-jq --arg project "$PROJECT" '.main = "dist/index.js" | .name = $project' package.json | tee package.json.new
+jq --arg project "$PROJECT" '.main = "dist/index.js" | .type = "module" | .name = $project' package.json | tee package.json.new
 mv package.json.new package.json
-npm install --save-dev typescript tslint
+npm install --save-dev typescript eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
 mkdir -p src
 touch src/index.ts
-npx tsc --init --rootDir src --outDir dist
-npx tslint --init
+npx tsc --init --module es2022 --target es2022 --rootDir src --outDir dist
+
+cat << EOF > .eslintrc.cjs
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended'
+  ],
+};
+EOF
+
+cat << EOF > .eslintignore
+dist/
+EOF
+
 npm install --save-dev \
     @types/node \
     nodemon \
@@ -19,5 +39,5 @@ npm install --save-dev \
 npm pkg set scripts.build="tsc"
 npm pkg set scripts.start="node dist/index.js"
 npm pkg set scripts.dev="nodemon --watch 'src/**/*.ts' --exec 'ts-node' src/index.ts"
-npm pkg set scripts.lint="tslint -c tslint.json 'src/**/*.ts'"
+npm pkg set scripts.lint="eslint .'"
 
